a99aaf5b60e6c81759c55768fffd217c1217955b
Brunno <bmonte@outlook.com>
2021-06-01 09:50:50 -0300

    Corrigir hook de filtros

--- src/hooks/useFilters/index.tsx
+++ src/hooks/useFilters/index.tsx
@@ -82,10 +82,6 @@ export function FiltersProvider({ children }: FiltersProviderProps) {
   }, [transactions, filters]);
 
   useEffect(() => {
-    setFilteredTransactions(transactions);
-  }, [transactions]);
-
-  useEffect(() => {
     applyFilters();
   }, [applyFilters]);
 
